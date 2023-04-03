fd8e575b1889bce157630dfa291b82d39b8c3c8a
zeynepakkaya-1 <125153348+zeynepakkaya-1@users.noreply.github.com>
2023-04-03 12:12:33 +0300

    IsPrimeNumber function

---
+++ Examples/PrimeNumber.cs
@@ -0,0 +1,17 @@
+﻿static bool IsPrimeNumber(int number)
+{
+    bool result = true;
+
+    for(int i = 2; i < number-1; i++)
+    {
+        if(number % i == 0)
+        {
+            result = false;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine(IsPrimeNumber(100));
+Console.WriteLine(IsPrimeNumber(17));
+Console.WriteLine(IsPrimeNumber(7));
